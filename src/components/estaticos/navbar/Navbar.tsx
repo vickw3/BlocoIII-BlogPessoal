aa2e9a5a0bae44c98a4a7465166f6cd6b3474be6
vickw3 <victoria.alcoforado@gmail.com>
2022-09-05 00:21:08 -0300

    Adição de Home e Login, Alteração Navbar, Footer e App.tsx

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,49 +1,56 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+import './Navbar.css';
 
 function Navbar() {
-    return (
-        <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer" }}>
-                        <Typography variant="h5" color="inherit">
-                            BlogPessoal
-                        </Typography>
-                    </Box>
+  return (
+    <>
+      <AppBar
+        position="static"
+        style={{ background: "#74849C", color: "#F2F2F2" }}
+      >
+        <Toolbar variant="dense">
+          <Box style={{ cursor: "default" }}>
+            <Typography variant="h5" color="inherit">
+              BlogPessoal //
+            </Typography>
+          </Box>
 
-                    <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                home
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar temas
-                            </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                logout
-                            </Typography>
-                        </Box>
-                    </Box>
-
-                </Toolbar>
-            </AppBar>
-        </>
-    )
+          <Box display="flex" justifyContent="start">
+            <Box mx={1} className='cursor'>
+              <Typography variant="h6" color="inherit">
+                home
+              </Typography>
+            </Box>
+            <Box mx={1} className='cursor'>
+              <Typography variant="h6" color="inherit">
+                postagens
+              </Typography>
+            </Box>
+            <Box mx={1} className='cursor'>
+              <Typography variant="h6" color="inherit">
+                temas
+              </Typography>
+            </Box>
+            <Box mx={1} className='cursor'>
+              <Typography variant="h6" color="inherit">
+                cadastrar tema
+              </Typography>
+            </Box>
+            <Link to="/login" className="text-decorator-none">
+              <Box mx={1} className='cursor'>
+                <Typography variant="h6" color="inherit">
+                  logout
+                </Typography>
+              </Box>
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 
 export default Navbar;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
-import './Home.css';
+import "./Home.css";
+
+import { Grid, Button, Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 
 function Home() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+  return (
+    <>
+      <Grid container direction="row" justifyContent="center" alignItems="center" className="container">
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo-h3">Seja bem-vinde!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo-h5">Blog pessoal com meus estudos sobre linguagens de programação e tecnologias.</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                        <Button variant="outlined" className="btn">Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                    <img src="https://i.imgur.com/1RwWtgp.png" alt="" width="790px" height="512px" />
                 </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                <Grid xs={12} className='postagens'>
+                  
                 </Grid>
             </Grid>
-        </>
-    );
+    </>
+  );
 }
 
 export default Home;
